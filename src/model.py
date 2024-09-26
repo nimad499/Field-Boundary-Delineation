76cb8426fd131da1deddb3ae97ea2b1b608ac313
nimad499 <nimad499@outlook.com>
2024-09-26 12:44:36 +0330

    add deeplabv3 model

--- src/model.py
+++ src/model.py
@@ -1,6 +1,8 @@
+import torch
 from torchvision.models.detection import maskrcnn_resnet50_fpn_v2
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torchvision.models.segmentation import deeplabv3_resnet50
 
 # ToDo: Add option to set the number of channels
 
@@ -15,6 +17,18 @@ def mask_rcnn_model():
     dim_reduced = model.roi_heads.mask_predictor.conv5_mask.out_channels
     model.roi_heads.mask_predictor = MaskRCNNPredictor(
         in_features_mask, dim_reduced, 2
+    )
+
+    return model
+
+
+def deeplabv3_model():
+    model = deeplabv3_resnet50(weights="DEFAULT")
+
+    in_channels = model.classifier[4].in_channels
+    kernel_size = model.classifier[4].kernel_size
+    model.classifier[4] = torch.nn.Conv2d(
+        in_channels, 2, kernel_size=kernel_size
     )
 
     return model
