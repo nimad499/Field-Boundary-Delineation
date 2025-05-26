fba477d0d986ca99dc845bc95523996cf6a2a6dd
nimad499 <nimad499@outlook.com>
2025-05-27 00:14:08 +0330

    avoid changing default detections_per_img

--- src/model.py
+++ src/model.py
@@ -12,7 +12,7 @@ def mask_rcnn_model():
 
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 2)
-    model.roi_heads.detections_per_img = 600
+    # model.roi_heads.detections_per_img = 600
 
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
     dim_reduced = model.roi_heads.mask_predictor.conv5_mask.out_channels
